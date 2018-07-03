2a6a1a10b0261b20cfa7d81b4ea7f2ea4b724734
Pethical <sokkis@gmail.com>
2018-07-03 19:51:19 +0200

    Intiial commit

---
+++ MicroCoinGPUMiner/MicroCoinGPUMiner/Program.cs
@@ -0,0 +1,249 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicroCoinGPUMiner
+{
+    class Program
+    {
+
+        static bool CanBeModifiedOnLastChunk(long MessageTotalLength, ref long StartBytePos)
+        {
+            StartBytePos = (((((MessageTotalLength) * 8) + 72) % 512) / 8) - (8 + 9);
+            return (StartBytePos >= 0) && ((StartBytePos % 4) == 0) && (StartBytePos <= 48);
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+        static string d, p, f, h;
+        static int b;
+        static string stdate = Convert.ToString(DateTime.Now);
+        static void consolwriter()
+        {
+            Console.CursorVisible = false;
+            while (true)
+            {
+                try
+                {
+                    string stdate2 = Convert.ToString(DateTime.Now);
+                    var jo = JObject.Parse(d).GetValue("params").Value<JArray>();
+                    var n = jo[0].Value<JObject>();
+                    Console.Clear();
+                    string test = "Block: " + n.GetValue("block").Value<int>()+ "\n" +
+                        "timestamp: "+ n.GetValue("timestamp").Value<string>()+"\n" +
+                        "target_pow: "+ n.GetValue("target_pow").Value<string>()+"\n" +
+                        "target: " + n.GetValue("target").Value<int>().ToString("X") + "\n" +
+                        "Blocks Found: "+ Convert.ToString(b)+"\n" +
+                        ""+"\n" +
+                        Encoding.ASCII.GetString(StringToByteArray(p))+"\n"+
+                        "Elapsed time: "+Convert.ToString(DateTime.Parse(stdate2) - DateTime.Parse(stdate)) +"\n" +
+                        "Last block found: "+ f;
+                    Console.Write(test);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Thread.Sleep(999);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 10;
+            Console.WindowWidth = 80;
+            Console.SetBufferSize(80, 10);
+            string minerName = "";
+            bool startth = true;
+            Console.Title = "CuDaMiner";
+            
+            try
+            {
+                if (args.Length < 2)
+                {
+                    minerName = "mcu.dll";
+
+                    Process miner = Process.Start(new ProcessStartInfo(minerName)
+                    {
+                        UseShellExecute = false
+                    });
+                }
+                else
+                {
+                    minerName = args[1];
+                }
+            }
+            catch
+            {
+                Console.WriteLine("missing dll file");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            while (true)
+            {
+                File.WriteAllText(".\\datain00.txt", "$00000000\n$00000000");
+                try {
+                    using (TcpClient tcp = new TcpClient(AddressFamily.InterNetwork))
+                    {
+                        tcp.NoDelay = true;
+                        tcp.ExclusiveAddressUse = true;
+                        tcp.Connect("127.0.0.1", 4009);
+                        while (tcp.Available < 1)
+                        {
+                            Thread.Sleep(1);
+                        }
+
+                        StreamReader reader = new StreamReader(tcp.GetStream());
+                        string lastTimeStamp = "", lastNonce = "";
+                        string part1 = "";
+                        string payload = "";
+                        string payload_start = "";
+                        string part3 = "";
+                        string header = "";
+                        lastNonce = "00000000";
+                        StreamWriter sw = new StreamWriter(tcp.GetStream(), Encoding.ASCII);
+                        string ftime = "";
+                        while (true)
+                        {
+                            if (!tcp.Connected) break;
+                            if (tcp.Available > 0)
+                            {
+                                string data = reader.ReadLine();
+
+                                try
+                                {
+                                    var jo = JObject.Parse(data).GetValue("params").Value<JArray>();
+                                    var n = jo[0].Value<JObject>();
+                                    
+                                    if (startth == true)
+                                    {
+                                        Thread cw = new Thread(consolwriter);
+                                        cw.Start();
+                                        startth = false;
+                                    }
+
+                                    part1 = n.GetValue("part1").Value<string>();
+                                    payload = n.GetValue("payload_start").Value<string>();
+                                    part3 = n.GetValue("part3").Value<string>();
+                                    header = part1 + payload + part3;
+                                    long j = 0;
+                                    payload_start = n.GetValue("payload_start").Value<string>();
+                                    string u = n.GetValue("target_pow").Value<string>().Substring(8, 8);
+                                    int intAgain = int.Parse(u, System.Globalization.NumberStyles.HexNumber)-1;
+                                    header = part1 + payload + part3;
+                                    while (!CanBeModifiedOnLastChunk(8 + (header.Length / 2), ref j))
+                                    {
+                                        payload = payload + BitConverter.ToString(new byte[] { (byte)'-' });
+                                        header = part1 + payload + part3;
+                                    }
+
+                                    f = ftime;
+                                    h = header;
+                                    p = payload;
+                                    d = data;
+                                    for (int i = 0; i < 10; i++)
+                                    {
+                                        try
+                                        {
+                                            File.WriteAllText("./headerout00.txt", header);
+                                            File.WriteAllText("./targetpow.tp", Convert.ToString(intAgain));
+                                            break;
+                                        }
+                                        catch { }
+                                    }
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine(e.Message);
+                                }
+                            }
+                            string[] str = null;
+                            for (int i = 0; i < 10; i++)
+                            {
+                                try
+                                {
+                                    str = File.ReadAllLines(".\\datain00.txt");
+                                    break;
+                                }
+                                catch { }
+                            }
+                            if (str == null) return;
+                            str[0] = str[0].Replace("$", "");
+                            str[1] = str[1].Replace("$", "");
+                            if (str[0] != lastNonce)
+                            {
+                                lastNonce = str[0];
+                                string toSend = "{\"method\":\"miner-submit\",\"params\":[{\"payload\":\"" + payload + "\",\"timestamp\":" +
+                                    Convert.ToString(Convert.ToInt32(str[1], 16)) + ",\"nonce\":" + Convert.ToString(Convert.ToInt32(lastNonce, 16)) + "}]}";
+                                sw.WriteLine(toSend);
+                                sw.Flush();
+                                while (tcp.Available < 1) Thread.Sleep(10);
+                                StringBuilder sb = new StringBuilder();
+                                while (tcp.Available > 0)
+                                {
+                                    sb.Append(reader.ReadLine());
+                                }
+
+                                JObject js = JObject.Parse(sb.ToString());
+                                try
+                                {
+                                    if (js["error"] != null)
+                                    {
+                                        if (js.Value<string>("error").Length > 0)
+                                        {
+                                            Console.WriteLine(js.Value<string>("error"));
+                                            continue;
+                                        }
+                                    }
+                                }
+                                catch
+                                {
+
+                                }
+                                ftime = Convert.ToString(DateTime.Now);
+                                //System.Media.SoundPlayer player = new System.Media.SoundPlayer("found.wav");
+                                //player.Play();
+                                b++;
+                                string stdate2 = Convert.ToString(DateTime.Now);
+                                var jo = JObject.Parse(d).GetValue("params").Value<JArray>();
+                                var n = jo[0].Value<JObject>();
+                                Console.Clear();
+                                string test = "Block: " + n.GetValue("block").Value<int>() +"\n" +
+                                    "timestamp: " + n.GetValue("timestamp").Value<string>() + "\n" +
+                                    "target_pow: " + n.GetValue("target_pow").Value<string>() + "\n" +
+                                    "target: " + n.GetValue("target").Value<int>().ToString("X") + "\n" +
+                                    "Blocks Found: " + Convert.ToString(b) + "\n" +
+                                    "" + "\n" +
+                                    Encoding.ASCII.GetString(StringToByteArray(p)) + "\n" +
+                                    "Elapsed time: " + Convert.ToString(DateTime.Parse(stdate2) - DateTime.Parse(stdate)) + "\n" +
+                                    "Last block found: " + f;
+                                Console.Write(test);
+
+                            }
+
+                            Thread.Sleep(200);
+                        }
+                    }
+                }
+                catch(Exception re)
+                {
+                    Console.WriteLine(re.Message);
+
+                }
+            }
+        }
+    }
+}
---
+++ cuda miner/miner/kernel.cu
@@ -0,0 +1,661 @@
+
+#include "cuda_runtime.h"
+#include <cstdint>
+#include "device_launch_parameters.h"
+#include <chrono>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <ctime>
+
+#ifdef __INTELLISENSE__
+#define __launch_bounds__(blocksize)
+#endif
+
+cudaStream_t cudastream;
+static uint32_t hextarpow = 0xFFFFFFFF;
+static char tarpow[11];
+uint32_t *blockHeadermobj = nullptr;
+uint32_t *midStatemobj = nullptr;
+uint32_t *nonceOutmobj = nullptr;
+
+__device__ __forceinline__ uint32_t ror(const uint32_t a, const unsigned int n)
+{
+#if __CUDA_ARCH__ >= 350
+	return __funnelshift_r((a), (a), (n));
+#else
+	return (a >> n) | (a << (32 - n));
+#endif
+}
+
+
+#define shr (a) >> (n);
+
+#define ROTRIGHT(a,b) ((a >> b) | (a << (32 - b)))
+#define SIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
+#define SIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
+#define SIG0c(x) (ror(x,7) ^ ror(x,18) ^ ((x) >> 3))
+#define SIG1c(x) (ror(x,17) ^ ror(x,19) ^ ((x) >> 10))
+
+#define blocksize 2048 
+#define npt 9
+
+static const uint32_t k[64] = {
+	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+	0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+	0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+	0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+	0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+	0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+	0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+	0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+	0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+
+__global__ void __launch_bounds__(blocksize, 8) nonceGrindc(uint32_t *const __restrict__ headerIn, uint32_t *const __restrict__ midstateIn, uint32_t *const __restrict__ nonceOut, uint32_t const hextarpowpo)
+{
+	static const uint32_t k[64] = {
+		0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+		0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+		0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+		0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+		0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+		0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+		0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+		0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+		0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+	};
+
+	// int i = (blockIdx.x * blockDim.x * blockDim.y) + threadIdx.x;
+	//i *= npt;
+
+	uint32_t buffer[16];
+	uint32_t midstate[8];
+
+	// const uint32_t id = (blockDim.x * blockIdx.x * blockIdx.x * blockIdx.x * headerIn[16] + threadIdx.x)*npt;
+	const uint32_t id = (headerIn[16] << 20) | (blockDim.x * blockIdx.x + threadIdx.x)*npt;
+
+	midstate[0] = midstateIn[0];
+	midstate[1] = midstateIn[1];
+	midstate[2] = midstateIn[2];
+	midstate[3] = midstateIn[3];
+	midstate[4] = midstateIn[4];
+	midstate[5] = midstateIn[5];
+	midstate[6] = midstateIn[6];
+	midstate[7] = midstateIn[7];
+
+	int j = 0;
+
+#pragma unroll
+	for (j = 0; j < 16; j++)
+	{
+		buffer[j] = headerIn[j];
+	}
+
+	uint32_t block[64];
+
+	uint32_t temp1;
+	uint32_t temp2;
+	uint32_t S0;
+	uint32_t S1;
+
+	uint32_t h0, h1, h2, h3, h4, h5, h6, h7;
+
+	uint32_t a, b, c, d, e, f, g, h;
+	for (int n = id; n < id + npt; n++)
+	{
+		h0 = midstate[0];
+		h1 = midstate[1];
+		h2 = midstate[2];
+		h3 = midstate[3];
+		h4 = midstate[4];
+		h5 = midstate[5];
+		h6 = midstate[6];
+		h7 = midstate[7];
+
+		a = h0;
+		b = h1;
+		c = h2;
+		d = h3;
+		e = h4;
+		f = h5;
+		g = h6;
+		h = h7;
+
+		buffer[11] = n;
+		// printf("Nonce being used: %d\n" + buffer[11]);
+
+#pragma unroll
+		for (j = 0; j < 16; j++)
+		{
+			block[j] = buffer[j];
+		}
+#pragma unroll
+		for (j = 16; j < 64; j++)
+		{
+			block[j] = block[j - 16] + block[j - 7] + SIG1c(block[j - 2]) + SIG0c(block[j - 15]);
+		}
+#pragma unroll
+		for (j = 0; j < 64; j++)
+		{
+			S1 = (ror(e, 6)) ^ (ror(e, 11)) ^ (ror(e, 25));
+			temp1 = h + S1 + ((e & f) ^ ((~e) & g)) + k[j] + block[j];
+			S0 = (ror(a, 2)) ^ (ror(a, 13)) ^ (ror(a, 22));
+			temp2 = S0 + (((a & b) ^ (a & c) ^ (b & c)));
+
+			h = g;
+			g = f;
+			f = e;
+			e = d + temp1;
+			d = c;
+			c = b;
+			b = a;
+			a = temp1 + temp2;
+		}
+
+		h0 += a;
+		h1 += b;
+		h2 += c;
+		h3 += d;
+		h4 += e;
+		h5 += f;
+		h6 += g;
+		h7 += h;
+
+		block[0] = h0;
+		block[1] = h1;
+		block[2] = h2;
+		block[3] = h3;
+		block[4] = h4;
+		block[5] = h5;
+		block[6] = h6;
+		block[7] = h7;
+		block[8] = 0x80000000;
+		block[9] = 0x00000000;
+		block[10] = 0x00000000;
+		block[11] = 0x00000000;
+		block[12] = 0x00000000;
+		block[13] = 0x00000000;
+		block[14] = 0x00000000;
+		block[15] = 0x00000100;
+
+		h0 = a = 0x6a09e667;
+		h1 = b = 0xbb67ae85;
+		h2 = c = 0x3c6ef372;
+		h3 = d = 0xa54ff53a;
+		h4 = e = 0x510e527f;
+		h5 = f = 0x9b05688c;
+		h6 = g = 0x1f83d9ab;
+		h7 = h = 0x5be0cd19;
+
+#pragma unroll
+		for (j = 16; j < 64; j++)
+		{
+			block[j] = block[j - 16] + block[j - 7] + SIG1c(block[j - 2]) + SIG0c(block[j - 15]);
+		}
+
+#pragma unroll
+		for (j = 0; j < 64; j++)
+		{
+			S1 = (ror(e, 6)) ^ (ror(e, 11)) ^ (ror(e, 25));
+			temp1 = h + S1 + ((e & f) ^ ((~e) & g)) + k[j] + block[j];
+			S0 = (ror(a, 2)) ^ (ror(a, 13)) ^ (ror(a, 22));
+			temp2 = S0 + (((a & b) ^ (a & c) ^ (b & c)));
+
+			h = g;
+			g = f;
+			f = e;
+			e = d + temp1;
+			d = c;
+			c = b;
+			b = a;
+			a = temp1 + temp2;
+		}
+
+		h0 += a;
+		h1 += b;
+		h2 += c;
+		h3 += d;
+		h4 += e;
+		h5 += f;
+		h6 += g;
+		h7 += h;
+
+		if ((h0 == 0) && (h1 <= hextarpowpo))
+	//	if ((h0 <= 0x0000000f) && (h1 <= 0xffffffff))
+		{
+			*nonceOut = n;
+			/* Uncomment these for additional mining information/verbosity */
+	//		printf("    The hash is: \n%08x %08x %08x %08x %08x %08x %08x %08x \n Target Pow check: %08x \n", h0, h1, h2, h3, h4, h5, h6, h7, hextarpowpo);
+		
+		}
+	}
+}
+
+#define headerSize 176
+
+unsigned char* hexToByteArray(const char* hexstring)
+{
+	size_t len = headerSize * 2;
+	size_t final_len = len / 2;
+	unsigned char* chrs = (unsigned char*)malloc((final_len) * sizeof(*chrs));
+	for (size_t i = 0, j = 0; j<final_len; i += 2, j++)
+		chrs[j] = (hexstring[i] % 32 + 9) % 25 * 16 + (hexstring[i + 1] % 32 + 9) % 25;
+	return chrs;
+}
+
+char hex[headerSize * 2 + 1];
+int increment = 0;
+int callInc = 0;
+char old[4] = { 0x00, 0x00, 0x00, 0x00 };
+
+
+bool different(char* one, char* two, int length)
+{
+	int i = 0;
+	for (; i < length; i++)
+	{
+		if (one[i] != two[i]) return true;
+	}
+	return false;
+}
+
+int deviceToUse = 0;
+void getHeaderForWork(uint8_t *header)
+{
+	if (callInc % 200 == 0)
+	{
+		callInc = 0;
+		FILE *fr;
+
+		char fileName[16] = "headeroutXX.txt";
+		fileName[9] = (deviceToUse / 10) + 48;
+		fileName[10] = (deviceToUse % 10) + 48;
+
+		fr = fopen(fileName, "rt");
+		fgets(hex, 352, fr);
+		hex[headerSize*2] = '\0';
+
+		if (different(old, hex, 4))
+		{
+			old[0] = hex[0];
+			old[1] = hex[1];
+			old[2] = hex[2];
+			old[3] = hex[3];
+
+			printf(" Real: %s\n", hex);
+		}
+
+		fclose(fr);
+	}
+
+	callInc++;
+	unsigned char* bufferHeader = hexToByteArray(hex);
+	memcpy(header, bufferHeader, headerSize);
+	free(bufferHeader);
+	increment++;
+	long time = std::time(0);
+
+	header[168] = (time & 0x000000FF);
+	header[169] = (time & 0x0000FF00) >> 8;
+	header[170] = (time & 0x00FF0000) >> 16;
+	header[171] = (time & 0xFF000000) >> 24;
+}
+
+int hash = 0;
+int totalNonces = 0;
+int offset = 0;
+void grindNonces()
+{
+	static bool init = false;
+	static uint32_t *nonceOut = nullptr;
+	static uint8_t *blockHeader = nullptr;
+	static uint8_t *midState = nullptr;
+	cudaError_t ret;
+
+	if (!init)
+	{
+		ret = cudaMallocHost(&nonceOut, 4);
+		if (ret != cudaSuccess)
+		{
+			printf("ERROR ALLOCATION\n");
+		}
+		ret = cudaMallocHost(&blockHeader, 64);
+		if (ret != cudaSuccess)
+		{
+			printf("ERROR ALLOCATION\n");
+		}
+		ret = cudaMallocHost(&midState, 32);
+		if (ret != cudaSuccess)
+		{
+			printf("ERROR ALLOCATION\n");
+		}
+		init = true;
+	}
+
+	int i;
+
+	getHeaderForWork(blockHeader);
+	*nonceOut = 0;
+
+	uint32_t block[64];
+
+	uint32_t h0 = 0x6a09e667;
+	uint32_t h1 = 0xbb67ae85;
+	uint32_t h2 = 0x3c6ef372;
+	uint32_t h3 = 0xa54ff53a;
+	uint32_t h4 = 0x510e527f;
+	uint32_t h5 = 0x9b05688c;
+	uint32_t h6 = 0x1f83d9ab;
+	uint32_t h7 = 0x5be0cd19;
+
+	uint32_t a = h0;
+	uint32_t b = h1;
+	uint32_t c = h2;
+	uint32_t d = h3;
+	uint32_t e = h4;
+	uint32_t f = h5;
+	uint32_t g = h6;
+	uint32_t h = h7;
+
+	for (int i = 0; i < 16; i++)
+	{
+		block[i] = ((uint32_t)blockHeader[i * 4 + 0] << 24) | ((uint32_t)blockHeader[i * 4 + 1] << 16) | ((uint32_t)blockHeader[i * 4 + 2] << 8) | ((uint32_t)blockHeader[i * 4 + 3]);
+	}
+
+	for (int i = 16; i < 64; i++)
+	{
+		block[i] = block[i - 16] + block[i - 7] + SIG1(block[i - 2]) + SIG0(block[i - 15]);
+	}
+
+	uint32_t temp1;
+	uint32_t temp2;
+	uint32_t S1;
+	uint32_t S0;
+
+	for (int i = 0; i < 64; i++)
+	{
+		S1 = (ROTRIGHT(e, 6)) ^ (ROTRIGHT(e, 11)) ^ (ROTRIGHT(e, 25));
+		temp1 = h + S1 + ((e & f) ^ ((~e) & g)) + k[i] + block[i];
+		S0 = (ROTRIGHT(a, 2)) ^ (ROTRIGHT(a, 13)) ^ (ROTRIGHT(a, 22));
+		temp2 = S0 + (((a & b) ^ (a & c) ^ (b & c)));
+
+		h = g;
+		g = f;
+		f = e;
+		e = d + temp1;
+		d = c;
+		c = b;
+		b = a;
+		a = temp1 + temp2;
+	}
+
+	h0 += a;
+	h1 += b;
+	h2 += c;
+	h3 += d;
+	h4 += e;
+	h5 += f;
+	h6 += g;
+	h7 += h;
+
+	a = h0;
+	b = h1;
+	c = h2;
+	d = h3;
+	e = h4;
+	f = h5;
+	g = h6;
+	h = h7;
+
+	for (int i = 0; i < 16; i++)
+	{
+		block[i] = ((uint32_t)blockHeader[(i + 16) * 4 + 0] << 24) | ((uint32_t)blockHeader[(i + 16) * 4 + 1] << 16) | ((uint32_t)blockHeader[(i + 16) * 4 + 2] << 8) | ((uint32_t)blockHeader[(i + 16) * 4 + 3]);
+	}
+
+	for (int i = 16; i < 64; i++)
+	{
+		block[i] = block[i - 16] + block[i - 7] + SIG1(block[i - 2]) + SIG0(block[i - 15]);
+	}
+
+	for (int i = 0; i < 64; i++)
+	{
+		S1 = (ROTRIGHT(e, 6)) ^ (ROTRIGHT(e, 11)) ^ (ROTRIGHT(e, 25));
+		temp1 = h + S1 + ((e & f) ^ ((~e) & g)) + k[i] + block[i];
+		S0 = (ROTRIGHT(a, 2)) ^ (ROTRIGHT(a, 13)) ^ (ROTRIGHT(a, 22));
+		temp2 = S0 + (((a & b) ^ (a & c) ^ (b & c)));
+
+		h = g;
+		g = f;
+		f = e;
+		e = d + temp1;
+		d = c;
+		c = b;
+		b = a;
+		a = temp1 + temp2;
+	}
+
+	h0 += a;
+	h1 += b;
+	h2 += c;
+	h3 += d;
+	h4 += e;
+	h5 += f;
+	h6 += g;
+	h7 += h;
+
+	uint32_t midstateInternal[8];
+
+	midstateInternal[0] = h0;
+	midstateInternal[1] = h1;
+	midstateInternal[2] = h2;
+	midstateInternal[3] = h3;
+	midstateInternal[4] = h4;
+	midstateInternal[5] = h5;
+	midstateInternal[6] = h6;
+	midstateInternal[7] = h7;
+
+	uint32_t remainingHeader[17];
+	for (int i = 0; i < 12; i++)
+	{
+		remainingHeader[i] = ((uint32_t)blockHeader[(i + 32) * 4 + 0] << 24) | ((uint32_t)blockHeader[(i + 32) * 4 + 1] << 16) | ((uint32_t)blockHeader[(i + 32) * 4 + 2] << 8) | ((uint32_t)blockHeader[(i + 32) * 4 + 3]);
+	}
+	remainingHeader[12] = 0x80000000;
+	remainingHeader[13] = 0x00000000;
+	remainingHeader[14] = 0x00000000;
+	remainingHeader[15] = 0x00000580;
+
+
+	remainingHeader[16] = ++offset;
+
+	cudaMemcpyAsync(midStatemobj, midstateInternal, 32, cudaMemcpyHostToDevice, cudastream);
+	cudaMemcpyAsync(nonceOutmobj, nonceOut, 4, cudaMemcpyHostToDevice, cudastream);
+	cudaMemcpyAsync(blockHeadermobj, remainingHeader, 68, cudaMemcpyHostToDevice, cudastream);
+	nonceGrindc << < 128, 1024, blocksize, cudastream >> > (blockHeadermobj, midStatemobj, nonceOutmobj, hextarpow);
+	cudaMemcpyAsync(nonceOut, nonceOutmobj, 4, cudaMemcpyDeviceToHost, cudastream);
+	cudaStreamSynchronize(cudastream);
+
+	if (*nonceOut != 0)
+	{
+		uint32_t nonce = *nonceOut;
+		nonce = (((nonce & 0xFF000000) >> 24) | ((nonce & 0x00FF0000) >> 8) | ((nonce & 0x0000FF00) << 8) | ((nonce & 0x000000FF) << 24));
+		uint32_t timestamp = remainingHeader[10];
+		timestamp = ((timestamp & 0x000000FF) << 24) + ((timestamp & 0x0000FF00) << 8) + ((timestamp & 0x00FF0000) >> 8) + ((timestamp & 0xFF000000) >> 24);
+
+		FILE* f2;
+
+		char fileName[13] = "datainXX.txt";
+		fileName[6] = (deviceToUse / 10) + 48;
+		fileName[7] = (deviceToUse % 10) + 48;
+
+		f2 = fopen(fileName, "w");
+		while (f2 == NULL)
+		{
+			f2 = fopen(fileName, "w");
+		}
+
+		fprintf(f2, "\$%08x\n", nonce);
+		fprintf(f2, "\$%08x", timestamp);
+		fclose(f2);
+
+
+		*nonceOut = 0;
+		totalNonces++;
+	}
+
+}
+
+static void tpow()
+{
+	while (true)
+	{
+		char hex2[11];
+		FILE *fr2;
+
+		char fileName[13] = "targetpow.tp";
+
+		fr2 = fopen(fileName, "rt");
+		fgets(hex2, 11, fr2);
+		for (int i = 0; i < 11; i++)
+		{
+			tarpow[i] = hex2[i];
+		}
+		int num = (int)strtol(tarpow, NULL, 10);
+
+		hextarpow = static_cast<uint32_t>(num);
+		//printf("%x\n",hextarpow);
+		fclose(fr2);
+		Sleep(200);
+	}
+}
+int main(int argc, char *argv[])
+{
+	HANDLE Th1;
+	DWORD ThreadID;
+
+	Th1 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)tpow, NULL, 0, &ThreadID);
+
+	int i = 0; int j = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			char* argument = argv[i];
+			if (argument[0] == 'd')
+			{
+				deviceToUse = argument[1] - 48;
+			}
+		}
+	}
+
+	printf("Using Device: %d\n\n", deviceToUse);
+
+	unsigned int items_per_iter = 256 * 256 * 256 * 8;
+
+	unsigned int cycles_per_iter = 15;
+	double seconds_per_iter = 10.0;
+
+	int version, ret;
+	ret = cudaDriverGetVersion(&version);
+	if (ret != cudaSuccess)
+	{
+		printf("ERROR ALLOCATION\n");
+	}
+
+	int deviceCount;
+	ret = cudaGetDeviceCount(&deviceCount);
+	if (ret != cudaSuccess)
+	{
+		printf("ERROR ALLOCATION\n");
+	}
+
+	cudaDeviceProp deviceProp;
+
+	printf("CUDA Version: %.1f\n", ((float)version / 1000));
+	printf("CUDA Devices: %d\n", deviceCount);
+
+	printf("\n");
+
+	for (int count = 0; count < deviceCount; count++)
+	{
+		ret = cudaGetDeviceProperties(&deviceProp, count);
+		if (ret != cudaSuccess)
+		{
+			printf("ERROR ALLOCATION\n");
+		}
+		printf("Device #%d (%s):\n", count, deviceProp.name);
+		printf("    Clock Rate:              %d MHz\n", (deviceProp.clockRate / 1024));
+		printf("    Is Integrated:           %s\n", (deviceProp.integrated == 0 ? "false" : "true"));
+		printf("    Compute Capability:      %d.%d\n", deviceProp.major, deviceProp.minor);
+		printf("    Kernel Concurrency:      %d\n", deviceProp.concurrentKernels);
+		printf("    Max Grid Size:           %d\n", deviceProp.maxGridSize);
+		printf("    Max Threads per Block:   %d\n", deviceProp.maxThreadsPerBlock);
+		printf("    Registers per Block:     %d\n", deviceProp.regsPerBlock);
+		printf("    Registers per SM:        %d\n", deviceProp.regsPerMultiprocessor);
+		printf("    Processor Count:         %d\n", deviceProp.multiProcessorCount);
+		printf("    Shared Memory/Block:     %d\n", deviceProp.sharedMemPerBlock);
+		printf("    Shared Memory/Proc:      %d\n", deviceProp.sharedMemPerMultiprocessor);
+		printf("    Warp Size:               %d\n", deviceProp.warpSize);
+		printf("\n");
+	}
+
+	printf("Mining on device #%d...\n\n", deviceToUse);
+	ret = cudaSetDevice(deviceToUse);
+	if (ret != cudaSuccess)
+	{
+		printf("ERROR ALLOCATION\n");
+	}
+	cudaDeviceReset();
+	ret = cudaSetDeviceFlags(cudaDeviceScheduleBlockingSync);
+	if (ret != cudaSuccess)
+	{
+		printf("ERROR ALLOCATION\n");
+	}
+	ret = cudaStreamCreate(&cudastream);
+	if (ret != cudaSuccess)
+	{
+		printf("ERROR ALLOCATION\n");
+	}
+	ret = cudaMalloc(&blockHeadermobj, 68);
+	if (ret != cudaSuccess)
+	{
+		printf("ERROR ALLOCATION\n");
+	}
+	ret = cudaMalloc(&midStatemobj, 32);
+	if (ret != cudaSuccess)
+	{
+		printf("ERROR ALLOCATION\n");
+	}
+	ret = cudaMalloc(&nonceOutmobj, 4);
+	if (ret != cudaSuccess)
+	{
+		printf("ERROR ALLOCATION\n");
+	}
+
+
+	cudaError_t e = cudaGetLastError();
+	printf("Last error: %s\n", cudaGetErrorString(e));
+
+	bool quit = false;
+
+	while (!quit)
+	{
+		grindNonces();
+	}
+}
